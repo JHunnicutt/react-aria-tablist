bc9d02c6d58e2d946e83695706737995474ca28b
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2023-04-05 15:54:11 -0400

    resolved type errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,52 @@
-import { Key, ReactNode, useRef } from 'react';
+import { DOMAttributes, Key, ReactNode, useRef } from 'react';
 import { useTab, useTabList, useTabPanel } from 'react-aria';
 import {
 	Item,
-	TabListProps,
 	TabListState,
 	useTabListState,
+	TabListProps,
+	ItemProps,
 } from 'react-stately';
 
-function Tabs(props) {
+interface MyItemProps {
+	key: Key;
+	rendered: ReactNode;
+}
+
+interface TabProps {
+	key: Key;
+	item: MyItemProps;
+	state: TabListState<Object>;
+}
+
+function Tabs(props: TabListProps<Object>) {
 	let state = useTabListState(props);
+	let ref = useRef(null);
+	let { tabListProps } = useTabList(props, state, ref);
+
+	console.log(state);
+
+	return (
+		<div className="tabs">
+			<div {...tabListProps} ref={ref}>
+				{[...state.collection].map((item) => (
+					<Tab key={item.key} item={item} state={state} />
+				))}
+			</div>
+		</div>
+	);
+}
+
+function Tab({ item, state }: TabProps) {
+	let { key, rendered } = item;
+	let ref = useRef(null);
+	let { tabProps } = useTab({ key }, state, ref);
+
+	return (
+		<div {...tabProps} ref={ref}>
+			{rendered}
+		</div>
+	);
 }
 
 function App() {
